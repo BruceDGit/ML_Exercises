62dcc54fd8a2bd4f56ea641508f1e4d107c24251
Bruce <wayne_s@126.com>
2020-02-24 21:16:33 +0800

    linear regression

--- LR/linear_regression.py
+++ LR/linear_regression.py
@@ -10,5 +10,18 @@ w0, w1 = 1, 1
 lrate = 0.01
 
 for i in range(1, times+1):
+    # todo?
     d0 = (w0 + w1 * xs - ys).sum()
     d1 = (xs * (w0 + w1 * xs - ys)).sum()
+    w0 = w0 - lrate * d0
+    w1 = w1 - lrate * d1
+
+mp.figure("Linear Regression", facecolor='lightgray')
+mp.title('Linear Rgression', fontsize=18)
+mp.grid(linestyle=':')
+mp.scatter(xs, ys, s=80, color='dodgerblue', label='Samples')
+
+pred_y = w0 + w1 * xs
+mp.plot(xs, pred_y, color='orangered', label="Regression Line")
+mp.legend()
+mp.show()
