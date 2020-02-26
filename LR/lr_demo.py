cf77c40addfab8101f3f0a34ea4ad53e221fac95
Bruce <wayne_s@126.com>
2020-02-26 20:47:27 +0800

    linear regression demo and plottings

--- LR/lr_demo.py
+++ LR/lr_demo.py
@@ -19,6 +19,7 @@ for i in range(1, times+1):
     w0.append(w0[-1] - lrate * d0)
     w1.append(w1[-1] - lrate * d1)
 
+# Linear Regression
 mp.figure('Linear Regression', facecolor='lightgray')
 mp.title('Linear Regression', fontsize=18)
 mp.grid(linestyle=':')
@@ -27,7 +28,7 @@ pred_y = w0[-1] + w1[-1] * xs
 mp.plot(xs, pred_y, color='orangered', label='Regression Line')
 mp.legend()
 
-# subplot
+# subplot Training Progress
 w0 = w0[:-1]
 w1 = w1[:-1]
 
@@ -57,7 +58,40 @@ mp.grid(linestyle=':')
 mp.plot(epoches, losses, c='dodgerblue', label='loss')
 mp.legend()
 
-# todo Gradient Descent Line
+# Gradient Descent Line
+import mpl_toolkits.mplot3d as axes3d
+
+grid_w0, grid_w1 = np.meshgrid(
+    np.linspace(0, 9, 500),
+    np.linspace(0, 5, 500))
+
+grid_loss = np.zeros_like(grid_w0)
+for x, y in zip(xs, ys):
+    grid_loss += ((grid_w0 + x*grid_w1 - y) ** 2) / 2
+
+mp.figure('Loss Function')
+ax = mp.gca(projection='3d')
+mp.title('Loss Function', fontsize=20)
+ax.set_xlabel('w0', fontsize=14)
+ax.set_ylabel('w1', fontsize=14)
+ax.set_zlabel('loss', fontsize=14)
+ax.plot_surface(grid_w0, grid_w1, grid_loss, rstride=20, cstride=20, cmap='jet')
+ax.plot(w0, w1, losses, 'o-', c='orangered', label='BGD')
+mp.legend()
+
+# Gradient Descent Line2
+mp.figure('Batch Gradient Descent', facecolor='lightgray')
+mp.title('Batch Gradient Descent', fontsize=20)
+mp.xlabel('x', fontsize=14)
+mp.ylabel('y', fontsize=14)
+mp.tick_params(labelsize=10)
+mp.grid(linestyle=':')
+mp.contourf(grid_w0, grid_w1, grid_loss, 10, cmap='jet')
+cntr = mp.contour(grid_w0, grid_w1, grid_loss, 10, colors='black', linewidths=0.5)
+mp.clabel(cntr, inline_spacing=0.1, fmt='%.2f', fontsize=8)
+mp.plot(w0, w1, 'o-', c='orangered', label='BGD')
+mp.legend()
+
 
 mp.show()
 
